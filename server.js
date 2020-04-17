ede91e914332b260c4579303f5693edb149166b4
Gisele <giskey11@gmail.com>
2020-04-16 21:35:20 -0500

    fixing heroku issue

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ let MONGODB_URI=process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI,{
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+   useUnifiedTopology: true 
 });
 
 // routes
